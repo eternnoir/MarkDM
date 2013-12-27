3c33d4d7a21a69046fcad4fd56c85c5af6c642bb
FrankWang <eternnoir@gmail.com>
2013-12-27 21:46:48 +0800

    add requirements & change template to Jinja2

--- markdm/__init__.py
+++ markdm/__init__.py
@@ -23,7 +23,7 @@ def main(global_config, **settings):
             root_factory='markdm.models.RootFactory')
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/login')
